d6b9a57984bfb3066c4046f52db81a389a73df60
EmiNiemand <Szymon_PL@live.com>
2022-04-07 17:42:02 +0200

    Is this the end???

--- corrector.py
+++ corrector.py
@@ -1,16 +1,15 @@
-import random
 import bitarray as bitarray
 import numpy as np
 from bitarray import *
 
-# constant values, provides better readability
+# const values
 parity_bits = 8
 bits_in_byte = 8
 
 # Matrix which satisfies two conditions:
 # 1. No repeating rows
 # 2. Each row has unique sum of all of its values
-Hamming_matrix = np.array((
+H_matrix = np.array((
     [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
     [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -21,34 +20,31 @@ Hamming_matrix = np.array((
     [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]
 ))
 
-class CorrectionError(Exception):
-    pass
-
 #===============Main functions=====================
 
 # read bytes and encode each byte
 # using encode_byte()
-# and append first two bits
-def encode_bytes(bytes: bytearray):
+def encode_bytes(bytes):
     result = bytearray()
     for byte in bytes:
         bits = byte_to_bits(byte)
         encoded_byte = bytearray(encode_byte(bits).tobytes())
         result.append(encoded_byte[0])
         result.append(encoded_byte[1])
+
     return result
 
 
-# read every second byte from encoded_bytes
-def decode_bytes(encoded_bytes: bytearray):
+# read every second byte
+def decode_bytes(encoded_bytes):
     return encoded_bytes[::2]
 
 
-# read every second byte from encoded_bytes
-# combine info byte and guard byte together as bitarray
-# correct resulting bitarray via correct_byte()
-# and append first two bits at the end
-def correct_bytes(encoded_bytes: bytearray):
+# read every second byte info byte and guard byte
+# combine them together to bitarray
+# correct corrupted bit/bits in bitarray using correct_byte() function
+# and append these bytes at the end of a result
+def correct_bytes(encoded_bytes):
     result = bytearray()
     for i in range(0, len(encoded_bytes), 2):
         byte_as_bits = encoded_byte_to_bits(encoded_bytes[i], encoded_bytes[i + 1])
@@ -56,13 +52,14 @@ def correct_bytes(encoded_bytes: bytearray):
 
         result.append(corrected_byte[0])
         result.append(corrected_byte[1])
+
     return result
 
 
 #==============Other functions====================
 
 # convert byte to bits
-def byte_to_bits(byte: int) -> bitarray:
+def byte_to_bits(byte):
     result = ''
     for i in range(bits_in_byte):
         result += str(byte % 2)
@@ -71,35 +68,37 @@ def byte_to_bits(byte: int) -> bitarray:
 
 
 # convert two bytes to a single bitarray
-#   [a, b, c], [d, e, f] -> [a, b, c, d, e, f]
-def encoded_byte_to_bits(byte_one, byte_two) -> bitarray:
+def encoded_byte_to_bits(byte_one, byte_two):
     byte_as_bits = bitarray()
 
+    bits_one = byte_to_bits(byte_one)
+    bits_two = byte_to_bits(byte_two)
+
     for j in range(bits_in_byte):
-        byte_as_bits.append(byte_to_bits(byte_one)[j])
+        byte_as_bits.append(bits_one[j])
 
     for j in range(parity_bits):
-        byte_as_bits.append(byte_to_bits(byte_two)[j])
+        byte_as_bits.append(bits_two[j])
 
     return byte_as_bits
 
 
 # encode byte by adding parity byte:
-# slice hamming_matrix to have number of rows equal to number_of_parity_bits
+# get(number of parity bits) rows of H_matrix = new_matrix
+# new_array is just np.array of bits(converted from bitarray)
 # calculate parity byte:
-#   (hamming_matrix * T(word_array)) % 2
-# and finally combine information and parity byte:
-#   [a, b, c], [d, e, f] -> [a, b, c, d, e, f]
-def encode_byte(one_byte: bitarray) -> bitarray:
-    hamming_matrix = Hamming_matrix[0:, :parity_bits]
-    word_array = bitarray_to_nparray(one_byte)
+#   (new_matrix * T(new_array)) % 2
+# and finally combine original byte and parity byte
+def encode_byte(one_byte):
+    new_matrix = H_matrix[0:, :parity_bits]
+    new_array = bitarray_to_nparray(one_byte)
 
-    parity_byte = hamming_matrix.dot(word_array.T)
+    parity_byte = new_matrix.dot(new_array.T)
     parity_byte %= 2
 
     encoded_byte = bitarray()
     for i in range(bits_in_byte):
-        encoded_byte.append(word_array[i])
+        encoded_byte.append(new_array[i])
 
     for i in range(parity_bits):
         encoded_byte.append(parity_byte[i])
@@ -107,87 +106,92 @@ def encode_byte(one_byte: bitarray) -> bitarray:
     return encoded_byte
 
 # convert bitarray to np.array containing int from bits
-def bitarray_to_nparray(bits: bitarray) -> np.ndarray:
+def bitarray_to_nparray(bits):
     result = []
     for i in range(len(bits)):
         result.append(int(bits[i]))
+
     return np.array(result)
 
 
-# calculate: matrix_H * np.array(coded_byte) % 2
-# returns column that should be equal
-# to one of the matrix_H column
-def calculate_syndrome(coded_byte) -> np.ndarray:
+# calculate: H_matrix * np.array(coded_byte) % 2
+# return column equal
+# to one of the matrix_H column (if there is corrupted bit/bits)
+# otherwise return column of 0s
+def calculate_syndrome(coded_byte):
     syndrome_array = bitarray_to_nparray(coded_byte)
-    result: np.ndarray = Hamming_matrix.dot(syndrome_array)
+    result: np.ndarray = H_matrix.dot(syndrome_array)
+
     return result % 2
 
-# returns true if there are no 1's in array
-def check_coded_byte(syndrome: np.ndarray) -> bool:
+# returns true if syndrome is column of 0s
+def check_coded_byte(syndrome):
     return np.count_nonzero(syndrome) == 0
 
 
-# calculates coded_byte syndrome and then
+# calculates syndrome and then
 # tries to correct it
-def correct_byte(coded_byte: bitarray):
+def correct_byte(coded_byte):
     syndrome = calculate_syndrome(coded_byte)
     coded_byte_copy = coded_byte.copy()
 
     if check_coded_byte(syndrome):
         return coded_byte
+    if correct_one_bit(coded_byte_copy, syndrome) is not False:
+        return correct_one_bit(coded_byte_copy, syndrome)
+    if correct_two_bits(coded_byte_copy, syndrome) is not False:
+        return correct_two_bits(coded_byte_copy, syndrome)
+    else:
+        print("Something went wrong")
+        raise Exception
 
-    try:
-        return try_correct_one_bit(coded_byte_copy, syndrome)
-    except CorrectionError:
-        pass
-    try:
-        return try_correct_two_bits(coded_byte_copy, syndrome)
-    except CorrectionError:
-        raise CorrectionError()
-
-
-# checks if syndrome array has
-# corresponding column in transformed matrix_H
-# if so, flip the bit on position equal to column number
-# else raise error (there was more than one corrupted bit)
-def try_correct_one_bit(coded_byte: bitarray, syndrome: np.ndarray):
+# checks if syndrome has
+# corresponding column in transformed H_matrix
+# if so, change bit on position equal to column number
+# else return false (probably there are more corrupted bits)
+def correct_one_bit(coded_byte, syndrome):
     i = 0
-    for column in Hamming_matrix.T:
+    for column in H_matrix.T:
         # check if corresponding values of syndrome and column
         # are equal to each other
         if np.equal(syndrome, column).all():
-            coded_byte[i] ^= 1
+            if coded_byte[i] == 1:
+                coded_byte[i] = 0
+            else:
+                coded_byte[i] = 1
             return coded_byte
         i += i
 
-    raise CorrectionError()
+    return False
 
 
-# function sums two different rows of matrix_H,
-#  making sure each element has either 0 or 1 as value
-# if the syndrom and sum are identical, bits of coded_byte
-#  on positions of rows in sum are corrected
-# else raise error (there was more than two corrupted bits)
-def try_correct_two_bits(coded_byte: bitarray, syndrome: np.ndarray):
+# function sums two different rows of H_matrix,
+# then sum % 2 to make sure there's only bit values
+# if the syndrome and sum are identical,
+#  bits of coded_byte get corrected on positions of column1 and column2
+# else return false (probably there are more corrupted bits)
+def correct_two_bits(coded_byte, syndrome):
     i = 0
     j = 0
-    for column in Hamming_matrix.T:
-        for column2 in Hamming_matrix.T:
+    for column1 in H_matrix.T:
+        for column2 in H_matrix.T:
             if i == j:
                 continue
 
-            sum_of_two_columns = (column + column2) % 2
-            if np.equal(sum_of_two_columns, syndrome).all():
-                coded_byte[i] ^= 1
-                coded_byte[j] ^= 1
+            sum = (column1 + column2) % 2
+            if np.equal(sum, syndrome).all():
+                if coded_byte[i] == 1:
+                    coded_byte[i] = 0
+                else:
+                    coded_byte[i] = 1
+                if coded_byte[j] == 1:
+                    coded_byte[j] = 0
+                else:
+                    coded_byte[j] = 1
                 return coded_byte
             j += 1
         i += 1
         j = 0
 
-    raise CorrectionError()
-
-# return first 8 bits of bitarray
-def decode_byte(coded_byte: bitarray) -> bitarray:
-    return coded_byte[:8]
+    return False
 
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-import encode_decode as ed
+import corrector as c
+
+
+# read file as bytes and encode
+# save result to encoded.txt file
+def encode():
+    input_file = open("message.txt", 'rb').read()
+    encoded_file = c.encode_bytes(bytearray(input_file))
+    output_file = open("encoded.txt", 'wb')
+    output_file.write(encoded_file)
+
+
+# read file as bytes, correct bits and decode
+# save result to decoded.txt file
+def decode():
+    input_file = open("encoded.txt", 'rb').read()
+    encoded_file = c.correct_bytes(bytearray(input_file))
+    encoded_file = c.decode_bytes(encoded_file)
+    output_file = open("decoded.txt", 'wb')
+    output_file.write(encoded_file)
+
 
 # INFO
 # Input file name must be "message.txt"
 # Output file name after encoding is "encoded.txt";
 # Output file name after correction and decoding is "decoded.txt"
 def main():
-        user = str(input("Write e to encode or d to decode."))
-        if user == 'e':
-            ed.encode()
+    in_string = str(input("Write '1' to encode or '2' to decode."))
+    if in_string == '1':
+        encode()
 
-        elif user == 'd':
-            ed.decode()
+    elif in_string == '2':
+        decode()
 
 
 if __name__ == "__main__":
